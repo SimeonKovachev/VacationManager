19509035ee0b87c75e767d2ba14586c6db0d62dc
Simeon Kovachev <skcodingacc@gmail.com>
2023-04-20 12:56:35 +0300

    Client-side Front-end is done.

--- VacationManager.Entity/Project.cs
+++ VacationManager.Entity/Project.cs
@@ -15,9 +15,11 @@ namespace VacationManager.Entity
         public string Name { get; set; }
         [Display(Name = "Description")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Please choose Start Date")]
         [Display(Name = "Start Date")]
         [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "Please choose End Date")]
         [Display(Name = "End Date")]
         [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
